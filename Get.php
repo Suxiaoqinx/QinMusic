1f220c0e8c6bdba520e5bf5dc39941b152f4bf92
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2022-08-04 19:14:41 +0800

    Update Get.php

--- Get.php
+++ Get.php
@@ -10,9 +10,13 @@ define('HTTPS', true);
 //引入框架文件
 require 'Meting.php';
 use Metowolf\Meting;
-/************ ↓↓↓↓↓ 如果网易云音乐歌曲获取失效，请将你的放到这里！ ↓↓↓↓↓ ***************/
-$netease_cookie = "EVNSM=1.0.0; osver=11; deviceId=bnVsbAllMDpkYzpmZjpkYTpmODplNwllNmU1NTEyNjJiMGM1MTQ2CTIyOTNkYjI2OTRmMDg4NDg%3D; appver=8.2.61; NMDI=Q1NKTQkBDADsh7pcBCK8qgbSN5tUAAAAtazvAQoY74I493qrBeOSK1y976KWF670KVmkRMpPzTYB9JQqYa5g4%2BxJK31Q%2FbEwee4kMxVzFSOqGR2cbdIcVjAhREQW7DLq4lTMkWA%2BVUkLmUgN; MUSIC_U=00F953207D7D8109BC2B3997ED883887130769F3721815A2D4680FA09D51B8C063956B846EFDA797CF193BCFF2D528DD7C0DC329BED24D6DBB03E8633F951D252D906027367A299F1C08BD926DC5ABE423458133A6270D81E626FD27DFF6874346620536A14D6E824529287C109F9557E66422670DAF6F9AD5307C813821A9FD9221F0255426BB5CC5CBDAF133CC3BDF70EB3213F738433B76952A03B99EF67BF3FB07DE3A1B4C1FEEBA6B6708958AC949F8A995BD0985FEBC18009C4F452BBC3631904D74C1703A546B13EBFC55D548C496B9E8E77811A5532F59A87C20D8F2F3A9123AC2E6B1E661263A023CD2E79726D25FA038546EE906184A7106FCA5AD2FC6A1C2CAD990EAB07E0787D9BC97E7F8B1268CD3035AA98C998529398B2280C4A7B3A08BDDE23E0C46AE6B64972B1096D4D844A934CA4D3AB0A19782CA01D02A6AA06480975EB3C4226531A1447BC34295F1248B60CDC78D27C303F299372F33150E3D067E76C1E3456CD9D35A50152135D575886AA41488957FE9A888A3A9D2; ntes_kaola_ad=1; NMCID=vljatq.1627464858230.01.4; versioncode=8002061; mobilename=RedmiK20Pro; URS_APPID=4F768D0FF329351E82FB22AB87699622D883C47ADC85D19B8416DFD7BCD5BD8DB6F0E44087D61EFC06BE92279CD6EEC6; buildver=210721141452; resolution=2296x1080; __csrf=da05a11b61127b9326a970bf38225362; NMTID=00OaKRutU76MrxwQ0PUiyIdqKA1PwgAAAF67Hb_qA; os=android; channel=xiaomi";
-/************ ↑↑↑↑↑ 如果网易云音乐歌曲获取失效，请将你的放到这里！ ↑↑↑↑↑ ***************/
+/************ ↓↓↓↓↓ 如果网易云音乐会员歌曲获取失效，请将你获取到的cookie放到这里！ ↓↓↓↓↓ ***************/
+$netease_cookie = "";
+/************ ↑↑↑↑↑ 如果网易云音乐会员歌曲获取失效，请将你的cookie放到这里！ ↑↑↑↑↑ ***************/
+
+/************ ↓↓↓↓↓ 如果QQ音乐会员歌曲获取失效，请将你的cookie放到这里！ ↓↓↓↓↓ ***************/
+$tencent_cookie = "";
+/************ ↑↑↑↑↑ 如果QQ音乐会员歌曲获取失效，请将你的cookie放到这里！ ↑↑↑↑↑ ***************/
 /**
  * cookie 获取及使用方法见
  * https://github.com/mengkunsoft/MKOnlineMusicPlayer/wiki/%E7%BD%91%E6%98%93%E4%BA%91%E9%9F%B3%E4%B9%90%E9%97%AE%E9%A2%98
@@ -26,19 +30,23 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 		$type = $_GET['type'];
 		$media = $_GET['media'];
 		echo getMusicInfo($media,$type,$id);
+	}else{
+		echo json_encode(array('msg' => '相关参数设置不正确！'),320);
 	}
 }
-//网易云读取COOKIE 以防获取不到音乐
+//网易云需要写入COOKIE 以防获取不到音乐
 //海外服务器自行设置代理IP 以便获取QQ音乐
 function getMusicInfo($media = 'netease', $type ='song', $id = '') {
 	$api = new Meting($media);
 	//请自行设置代理IP 以便获取QQ音乐音频链接// 如果不需要请填写null 填写代理IP格式为 "IP:端口"
 	$proxy=null;
-	global $netease_cookie;
 	if ($media == 'netease') {
+		global $netease_cookie;
 		$api->cookie($netease_cookie);
 	}
 	if ($media == 'tencent') {
+		global $tencent_cookie;
+		$api->cookie($tencent_cookie);
 		$api->proxy($proxy);
 	}
 	$info = array();
@@ -72,8 +80,8 @@ function getMusicInfo($media = 'netease', $type ='song', $id = '') {
 			$url = str_replace('https://other.', 'http://other.', $url);
 		}
 		/**
-             * 修复QQ音乐HTTPS问题
-             */
+          *修复QQ音乐HTTPS问题
+        */
 		if ($media == 'tencent') {
 			//防止获取ws格式//
 			$url = str_replace("//ws","//isure", $url);
@@ -87,9 +95,19 @@ function getMusicInfo($media = 'netease', $type ='song', $id = '') {
 			//修复直链跳转问题//
 			header("Location:".$url);
 		}
-		$info[$keys] = array('name' => $data['name'],'url' => $url,'song_id' => $data['id'],'cover' => $cover,'author' => implode(' / ', $data['artist']),'lrc_data' => $lrc_data,'version' => '1.5.10');
+		if(!empty($url)){
+			//以免获取不到音乐链接//
+			$mp3url = $url;
+		}else{
+			$mp3url = '版权限制或该音乐不存在！';
+		}
+		$info[$keys] = array('name' => $data['name'],'url' => $mp3url,'song_id' => $data['id'],'cover' => $cover,'author' => implode(' / ', $data['artist']),'lrc_data' => $lrc_data,'version' => '1.5.10');
 	}
-	return json_encode($info,320|JSON_PRETTY_PRINT);
+	if(!empty($info)) {
+		return json_encode($info,320|JSON_PRETTY_PRINT);
+	} else {
+		return json_encode(array('msg' => '没有找到相关音乐'),320);
+	}
 }
 function lrctrim($lyrics) {
 	$result = "";
